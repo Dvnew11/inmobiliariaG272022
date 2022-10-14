a06dbca4d6a928a97b2dc5165d1b12aeab3350b0
Duvan Villanueva <111618561+Dvnew11@users.noreply.github.com>
2022-10-14 17:30:37 -0500

    Versión inicial de Inmobiliaria

---
+++ src/controllers/saludo.controller.ts
@@ -0,0 +1,14 @@
+// Uncomment these imports to begin using these cool features!
+
+import {get} from '@loopback/rest';
+
+
+export class SaludoController {
+  constructor() {}
+
+  //Método y ruta del servicio
+  @get('/saludar')
+  saludar(): string{
+    return 'Hola desde el servicio de saludar GET/Loopback'; 
+  }
+}
